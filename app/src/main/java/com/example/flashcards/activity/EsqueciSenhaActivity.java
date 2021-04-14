7d52a713e2748aa2ec42a6cbaa1d3fa25af0dfa3
marceloeugeniobruno <marceloeugeniobruno@gmail.com>
2021-04-14 10:53:48 -0300

    Valdações de campos não informados nas telas login e eqs

--- app/src/main/java/com/example/flashcards/activity/EsqueciSenhaActivity.java
+++ app/src/main/java/com/example/flashcards/activity/EsqueciSenhaActivity.java
@@ -4,18 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.flashcards.R;
+import com.example.flashcards.model.Usuario;
 
 public class EsqueciSenhaActivity extends AppCompatActivity {
+    private EditText campoEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_esqueci_senha);
+        campoEmail = findViewById(R.id.eqs_edt_email);
     }
 
     public void esqueciFinalizar(View view){
         finish();
+    }
+
+    public void sollicitarSenha(View view){
+        //recuperando textos
+        String email = campoEmail.getText().toString();
+        if(!email.isEmpty()){
+
+        }else{
+            Toast.makeText(EsqueciSenhaActivity.this,
+                    R.string.cad_msg_emailnaopreechido,
+                    Toast.LENGTH_LONG).show();
+        }
+
     }
 }
--- app/src/main/java/com/example/flashcards/activity/LoginActivity.java
+++ app/src/main/java/com/example/flashcards/activity/LoginActivity.java
@@ -5,15 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.flashcards.R;
+import com.example.flashcards.model.Usuario;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText campoEmail, campoSenha;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        campoEmail = findViewById(R.id.log_edt_email);
+        campoSenha = findViewById(R.id.log_edt_senha);
     }
     public void esquiciSenha(View view){
         Intent esqueci = new Intent(getApplicationContext(), EsqueciSenhaActivity.class);
@@ -24,4 +33,27 @@ public class LoginActivity extends AppCompatActivity {
         finish();
     }
 
+
+    public void validarLoginUsuario(View view){
+        //recuperando textos
+        String email = campoEmail.getText().toString();
+        String senha = campoSenha.getText().toString();
+        if(!email.isEmpty()){
+            if(!senha.isEmpty()){
+                 Usuario usuario = new Usuario();
+                 usuario.setEmail(email);
+                 usuario.setSenha(senha);
+            }else{
+                 Toast.makeText(LoginActivity.this,
+                       R.string.cad_msg_senhanaopreechido,
+                       Toast.LENGTH_LONG).show();
+            }
+        }else{
+                Toast.makeText(LoginActivity.this,
+                        R.string.cad_msg_emailnaopreechido,
+                        Toast.LENGTH_LONG).show();
+        }
+
+    }
 }
+
