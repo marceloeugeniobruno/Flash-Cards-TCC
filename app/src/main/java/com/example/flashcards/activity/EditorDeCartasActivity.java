563adc12f88d92b1c69fac5948509b43c52a74c7
marceloeugeniobruno <marceloeugeniobruno@gmail.com>
2021-05-09 18:20:02 -0300

    Validação de permissões

--- app/src/main/java/com/example/flashcards/activity/EditorDeCartasActivity.java
+++ app/src/main/java/com/example/flashcards/activity/EditorDeCartasActivity.java
@@ -1,15 +1,71 @@
 package com.example.flashcards.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import com.example.flashcards.R;
+import com.example.flashcards.helper.Permissoes;
 
 public class EditorDeCartasActivity extends AppCompatActivity {
+
+    private String [] permissoees = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor_de_cartas);
+        Permissoes.validarPermissoes(permissoees, this, 1);
+
+
+
+    }
+
+    public void edcFinalizar(View view){
+        finish();
+    }
+
+    public void edcSalvar(View view){
+        EditText textoFrente = findViewById(R.id.edc_edt_frente);
+
+        //TODO fazer código de salvamento
+        finish();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for (int permissaoResultado : grantResults){
+            if (permissaoResultado == PackageManager.PERMISSION_DENIED){
+                alertaValidacaoPermissoes();
+            }
+        }
+    }
+
+    private void alertaValidacaoPermissoes(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões Negadas");
+        builder.setMessage("Para utilizar adicionar uma carta é necessário Aceitar as permissões.");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
     }
 }
--- app/src/main/java/com/example/flashcards/activity/EditorDeCartasVersoActivity.java
+++ app/src/main/java/com/example/flashcards/activity/EditorDeCartasVersoActivity.java
@@ -3,16 +3,13 @@ package com.example.flashcards.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-
 import com.example.flashcards.R;
 
-public class EditorDeBaralhoActivity extends AppCompatActivity {
+public class EditorDeCartasVersoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_editor_de_baralho);
+        setContentView(R.layout.activity_editor_de_cartas_verso);
     }
-
 }
